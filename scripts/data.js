083b50ddf8485d698956c533a7bd65e4e9efc5d0
Henrique Dias <hacdias@gmail.com>
2019-04-30 17:31:41 +0100

    feat: cleanup (a lot)
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- scripts/data.js
+++ scripts/data.js
@@ -1,23 +1,34 @@
 const fs = require('fs')
-const path = require('path')
+const { join } = require('path')
 const yaml = require('node-yaml')
-const { sortAbc } = require('./utils')
-const dataDir = path.join(__dirname, '../data')
+const { sortAbc, sortInv, slugify } = require('./utils')
 
+const dir = join(__dirname, '../data')
 const trimIfExists = (str) => str ? str.trim() : undefined
 
-const files = fs.readdirSync(dataDir)
-  .map(file => path.join(dataDir, file))
+module.exports = fs.readdirSync(dir)
+  .map(file => join(dir, file))
   .map(file => yaml.readSync(file))
   .map(file => {
-    file.content = file.content.map(({ title, description, ...file }) => ({
+    file.slug = slugify(file.title)
+    file.type = 'category'
+
+    file.content = file.content.map(({ title, description, ...meta }, i) => ({
+      ...meta,
       title: trimIfExists(title),
       description: trimIfExists(description),
-      ...file
+      category: file.slug,
+      color: file.color,
+      index: i
     }))
 
+    let sort = (a, b) => sortAbc(a.title, b.title)
+
+    if (file.slug === 'articles') {
+      sort = (a, b) => sortInv(a.date, b.date)
+    }
+
+    file.content = file.content.sort(sort)
     return file
   })
   .sort((a, b) => sortAbc(a.title, b.title))
-
-module.exports = files
