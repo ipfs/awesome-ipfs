e8de79673809c8205928b7fe9191f71d0cb5656b
Daniel N <2color@users.noreply.github.com>
2023-09-18 12:21:02 +0200

    ci: fix github workflows

--- dangerfile.js
+++ dangerfile.js
@@ -2,34 +2,13 @@ const { danger, markdown, fail, warn, message } = require('danger')
 const awesomeBotResults = require('./ab-results-README.md-markdown-table.json')
 
 const githubMetadata = danger.git.fileMatch('.github/**')
-const yamlData = danger.git.fileMatch('data/**')
-const scripts = danger.git.fileMatch('scripts/**')
-const src = danger.git.fileMatch('src/**')
 const readme = danger.git.fileMatch('README.md')
 
-if (readme.edited) {
-  fail('Please do not edit the README directly. It is generated from the data in the data/ directory.')
+if (readme.edited && awesomeBotResults.error) {
+  fail(awesomeBotResults.title)
+  markdown(awesomeBotResults.message)
 }
 
 if (githubMetadata.edited) {
   message('Changes were made within the .github folder.')
-}
-
-if (yamlData.edited) {
-  message('YAML data files were edited.')
-  /**
-   * Only display awesomeBot results if the data yaml used to generate the readme have been modified.
-   */
-  if (awesomeBotResults.error) {
-    fail(awesomeBotResults.title)
-    markdown(awesomeBotResults.message)
-  }
-}
-
-if (scripts.edited) {
-  warn('Changes were made to the scripts folder.')
-}
-
-if (src.edited) {
-  warn('Changes were made to the src folder.')
 }
